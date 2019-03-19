5b0fac93df5a0458bf64be01849a98c71fa0ee63
NightlyD3V <TylerSpaulding95@gmail.com>
2019-03-19 22:00:15 +0100

    Completed MVP

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButtons = props => {
+    return (
+        <div>
+            <div>
+                <button className={props.buttonStyle}>÷</button>
+                <button className={props.buttonStyle}>x</button>
+                <button className={props.buttonStyle}>−</button>
+                <button className={props.buttonStyle}>+</button>
+                <button className={props.buttonStyle}>=</button>
+            </div>
+        </div>
+    )
+}
+
+export default ActionButtons
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+const Display = props => {
+    return (
+        <div className={props.displayStyle}>0</div>
+    );
+}
+
+export default Display
